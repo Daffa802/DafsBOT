2833631af96a3f602e1366d7fb1e52cf5702d773
Daffa802 <75373611+Daffa802@users.noreply.github.com>
2020-12-02 21:03:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const BotName = 'DAFS BOT☄'; 
+const instagram = 'https://instagram.com/Dafs_114.id'; 
 const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
+const kapanbotaktif = 'KETIKA OWNER ONLINE'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@Dafs_114.id`)
 })
 
 conn.on('message-new', async(m) =>
